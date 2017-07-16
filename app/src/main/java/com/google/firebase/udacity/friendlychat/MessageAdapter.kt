56694521417d9840f6651c64aa30ae9862615375
Nicolas Carrasco-Stevenson <niccarra@gmail.com>
2017-07-16 14:55:57 -0400

    Translate MessageAdapter into Kotlin

---
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MessageAdapter.kt
@@ -0,0 +1,41 @@
+package com.google.firebase.udacity.friendlychat
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_message.view.*
+
+/**
+ * Created by Nicolas Carrasco S on 7/16/2017.
+ */
+class MessageAdapter(
+        context: Context,
+        resource: Int,
+        objects: List<FriendlyMessage>) : ArrayAdapter<FriendlyMessage>(context, resource, objects) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View = convertView ?:
+                (context as Activity).layoutInflater.inflate(R.layout.item_message, parent, false)
+
+        val message = getItem(position)
+        if (!message.photoUrl.isEmpty()){
+            with(view){
+                messageTextView.visibility = View.GONE
+                photoImageView.visibility = View.VISIBLE
+                Picasso.with(context)
+                        .load(message.photoUrl)
+                        .into(photoImageView)
+            }
+        } else {
+            with(view){
+                messageTextView.visibility = View.VISIBLE
+                photoImageView.visibility = View.GONE
+                messageTextView.text = message.text
+            }
+        }
+        return view
+    }
+}
