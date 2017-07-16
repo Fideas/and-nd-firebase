f832042598ae89b35f40ab8ce5854b06df950786
Nicolas Carrasco-Stevenson <niccarra@gmail.com>
2017-07-16 14:34:09 -0400

    Translate FriendlyMessage into Kotlin

---
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/FriendlyMessage.kt
@@ -0,0 +1,6 @@
+package com.google.firebase.udacity.friendlychat
+
+/**
+ * Created by Nicolas Carrasco S on 7/16/2017.
+ */
+data class FriendlyMessage(val text: String, val name: String, val photoUrl: String)
